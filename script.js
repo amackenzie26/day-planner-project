35cdab39388b3624a04e4b71abc075e2da0c5f1f
Alec Mackenzie <alec.mackenzie26@gmail.com>
2021-09-30 19:11:23 -0700

    html/js updates

--- script.js
+++ script.js
@@ -3,39 +3,40 @@ const today = document.getElementById("currentDay");
 
 function showTime() {
     setInterval(() => {
-        today.innerText = moment().format("dddd, MMMM Do, YYYY");
     }, 1000)
 }
+today.innerText = moment().format("dddd, MMMM Do, YYYY");
+
 showTime();
 
 
 
-// Saving you input
-function saveInput() {
-    localStorage.setItem('inputSave', 'value');
-}
-saveInput()
+// // Saving user input
+// function saveInput() {
+//     localStorage.setItem('inputSave', 'value');
+// }
+// saveInput()
 
-function getValue() {
-    return localStorage.getItem('inputSave')
-}
-// console.log(getValue());
+// function getValue() {
+//     return localStorage.getItem('inputSave')
+// }
+// // console.log(getValue());
 
-document.getElementById('savebtn').addEventListener('click', saveInput);
-// console.log('save');
+// document.getElementById('savebtn').addEventListener('click', saveInput);
+// // console.log('save');
 
-function changeTimeBlockColor() {
-    document.col.style.background = red;
-}
+// function changeTimeBlockColor() {
+//     document.col.style.background = red;
+// }
 
-function applyClass() {
-    var d = new Date();
-    var n = d.getHours();
-    if (n > 9)
-$('timeofday').addClass('future');
-    else
-$('timeofday').addClass('present');
-    else
-$('timeofday').addClass('past');
-}
-col.on = applyClass
+// function applyClass() {
+//     var d = new Date();
+//     var n = d.getHours();
+//     if (n > 9)
+// $('timeofday').addClass('future');
+//     else
+// $('timeofday').addClass('present');
+//     else
+// $('timeofday').addClass('past');
+// }
+// col.on = applyClass
