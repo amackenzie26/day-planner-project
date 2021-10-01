31e87803355d5e24f44ccd319ab3e82557f7649d
Alec Mackenzie <alec.mackenzie26@gmail.com>
2021-09-30 23:46:27 -0700

    readme screen shots

--- script.js
+++ script.js
@@ -48,21 +48,3 @@ $("#5 .time-block").val(localStorage.getItem('5'))
 
 
 
-// // Saving user input
-// function saveInput() {
-//     localStorage.setItem('inputSave', 'value');
-// }
-// saveInput()
-
-// function getValue() {
-//     return localStorage.getItem('inputSave')
-// }
-// // console.log(getValue());
-
-// document.getElementById('savebtn').addEventListener('click', saveInput);
-// // console.log('save');
-
-// function changeTimeBlockColor() {
-//     document.col.style.background = red;
-// }
-
