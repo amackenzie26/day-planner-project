1afc39764c0c51ebd57b3cdc81b5483fc7189a60
Alec Mackenzie <alec.mackenzie26@gmail.com>
2021-09-30 18:54:10 -0700

    html/css/js updates

---
+++ script.js
@@ -0,0 +1,41 @@
+var currentTime = moment();
+const today = document.getElementById("currentDay");
+
+function showTime() {
+    setInterval(() => {
+        today.innerText = moment().format("dddd, MMMM Do, YYYY");
+    }, 1000)
+}
+showTime();
+
+
+
+// Saving you input
+function saveInput() {
+    localStorage.setItem('inputSave', 'value');
+}
+saveInput()
+
+function getValue() {
+    return localStorage.getItem('inputSave')
+}
+// console.log(getValue());
+
+document.getElementById('savebtn').addEventListener('click', saveInput);
+// console.log('save');
+
+function changeTimeBlockColor() {
+    document.col.style.background = red;
+}
+
+function applyClass() {
+    var d = new Date();
+    var n = d.getHours();
+    if (n > 9)
+$('timeofday').addClass('future');
+    else
+$('timeofday').addClass('present');
+    else
+$('timeofday').addClass('past');
+}
+col.on = applyClass
